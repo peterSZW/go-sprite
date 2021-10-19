09518175e2692fee77f178c58378d9608c1e1d89
Peter <peta@163.com>
2021-10-19 23:18:54 +0800

    Update sprite.go

--- sprite.go
+++ sprite.go
@@ -13,8 +13,10 @@ mySprite.Start()
 */package sprite
 
 import (
-	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
+	 
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	
 	"image"
 	"image/color"
 	"log"
